d1a8dc707a5ca52ec7de855cd5eaf2c3bf8ea8c1
Tianjie <xunchang@google.com>
2020-12-10 17:20:46 -0800

    Include the Pixel common board configuration for qcom
    Bug: 156162446
    Test: treehugger
    Change-Id: Iec908f4cef5d28c2f621e6d8bc845b9c0cb26761

--- target/board/BoardConfigCommon.mk
+++ target/board/BoardConfigCommon.mk
@@ -6,6 +6,7 @@ include $(SRC_EVERVOLV_DIR)/build/target/board/BoardConfigKernel.mk
 
 # Include vendor board platforms
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
+    include build/make/target/board/BoardConfigPixelCommon.mk
     include device/qcom/common/BoardConfigQcom.mk
 endif
 
