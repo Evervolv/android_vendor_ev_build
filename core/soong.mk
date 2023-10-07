99d1821d098a0fb33f216b4f0efbfc145352c903
maxwen <max.weninger@gmail.com>
2023-10-07 18:51:12 +0200

    soong: Follow the correct JSON syntax
    wrong: CROSS_COMPILE=\\"/data4...
    correct: CROSS_COMPILE=\"/data4...
    
    Change-Id: I5080aef7feb2ec7429cb9ba1f4baccefa61a3cd3

--- core/soong.mk
+++ core/soong.mk
@@ -21,7 +21,7 @@ SOONG_CONFIG_evervolvVarsPlugin :=
 
 define addVar
   SOONG_CONFIG_evervolvVarsPlugin += $(1)
-  SOONG_CONFIG_evervolvVarsPlugin_$(1) := $$(subst ",\",$$($1))
+  SOONG_CONFIG_evervolvVarsPlugin_$(1) := $($1)
 endef
 
 $(foreach v,$(EXPORT_TO_SOONG),$(eval $(call addVar,$(v))))
