8ea5e0df58d256e39c364e269d09a9775418ec99
Bruno Martins <bgcngm@gmail.com>
2023-10-25 00:53:43 +0100

    lineage: Move all QCOM board configs to hardware/qcom-caf/common
    Change-Id: I6ead35cf884255a8897a3e3e27fc7936d11be616

--- core/definitions.mk
+++ core/definitions.mk
@@ -1,3 +1,6 @@
 #
 # Evervolv specific macros
 #
+
+# Include board/platform macros
+include $(SRC_EVERVOLV_DIR)/build/core/utils.mk
--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -1,5 +1,3 @@
-# Target-specific configuration
-
 # Set device-specific HALs into project pathmap
 define set-device-specific-path
 $(if $(USE_DEVICE_SPECIFIC_$(1)), \
@@ -10,71 +8,23 @@ $(if $(USE_DEVICE_SPECIFIC_$(1)), \
 $(call project-set-path,qcom-$(2),$(strip $(path)))
 endef
 
-QC_OPEN_PATH := vendor/qcom/opensource
+ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
 
-ifeq ($(call is-board-platform-in-list, $(QCOM_BOARD_PLATFORMS)),true)
+$(call set-device-specific-path,AUDIO,audio,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/audio)
+$(call set-device-specific-path,DISPLAY,display,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/display)
+$(call set-device-specific-path,MEDIA,media,hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)/media)
 
-ifneq ($(filter $(A_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= msm8960
-else ifneq ($(filter $(B_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= msm8974
-else ifneq ($(filter $(B64_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= msm8994
-else ifneq ($(filter $(BR_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= msm8916
-else ifneq ($(filter $(UM_3_18_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= msm8996
-else ifneq ($(filter $(UM_4_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= msm8998
-else ifneq ($(filter $(UM_4_9_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= sdm845
-else ifneq ($(filter $(UM_4_14_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= sm8150
-else ifneq ($(filter $(UM_4_19_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= sm8250
-else ifneq ($(filter $(UM_5_4_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= sm8350
-else ifneq ($(filter $(UM_5_10_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT ?= sm8450
-else ifneq ($(filter $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-    QCOM_HARDWARE_VARIANT := sm8550
-endif
-QCOM_HARDWARE_VARIANT ?= $(TARGET_BOARD_PLATFORM)
-
-# Allow a device to opt-out hardset of PRODUCT_SOONG_NAMESPACES
-QCOM_SOONG_NAMESPACE ?= hardware/qcom-caf/$(QCOM_HARDWARE_VARIANT)
-
-$(call set-device-specific-path,AUDIO,audio,$(QCOM_SOONG_NAMESPACE)/audio)
-$(call set-device-specific-path,DISPLAY,display,$(QCOM_SOONG_NAMESPACE)/display)
-$(call set-device-specific-path,MEDIA,media,$(QCOM_SOONG_NAMESPACE)/media)
 $(call set-device-specific-path,BT_VENDOR,bt-vendor,hardware/qcom-caf/bt)
-ifneq ($(filter $(LEGACY_QCOM_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,$(QC_OPEN_PATH)/data-ipa-cfg-mgr-legacy-um)
-else
-$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,$(QC_OPEN_PATH)/data-ipa-cfg-mgr)
-endif
-$(call set-device-specific-path,DATASERVICES,dataservices,$(QC_OPEN_PATH)/dataservices)
+$(call set-device-specific-path,DATA_IPA_CFG_MGR,data-ipa-cfg-mgr,vendor/qcom/opensource/data-ipa-cfg-mgr)
+$(call set-device-specific-path,DATASERVICES,dataservices,vendor/qcom/opensource/dataservices)
 $(call set-device-specific-path,VR,vr,hardware/qcom-caf/vr)
 $(call set-device-specific-path,WLAN,wlan,hardware/qcom-caf/wlan)
 
-PRODUCT_SOONG_NAMESPACES += \
-    $(call project-path-for,qcom-data-ipa-cfg-mgr) \
-    $(call project-path-for,qcom-dataservices) \
-    $(QCOM_SOONG_NAMESPACE)
-
-# Add display-commonsys-intf to PRODUCT_SOONG_NAMESPACES for QSSI supported platforms
-ifneq ($(filter $(QSSI_SUPPORTED_PLATFORMS),$(TARGET_BOARD_PLATFORM)),)
-PRODUCT_SOONG_NAMESPACES += \
-    $(QC_OPEN_PATH)/commonsys-intf/display
-
-ifeq ($(filter $(UM_5_10_FAMILY) $(UM_5_15_FAMILY),$(TARGET_BOARD_PLATFORM)),)
-PRODUCT_SOONG_NAMESPACES += \
-    $(QC_OPEN_PATH)/commonsys/display
-endif
-
-endif
-
 PRODUCT_CFI_INCLUDE_PATHS += \
     hardware/qcom-caf/wlan/qcwcn/wpa_supplicant_8_lib
+
+else
+
+QC_OPEN_PATH := vendor/qcom/opensource
 
 endif
