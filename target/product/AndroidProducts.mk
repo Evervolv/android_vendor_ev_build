2b386c261c2e68885a2f7c18b38923d0a514ab0f
mse1969 <mse1969@posteo.de>
2024-03-24 23:20:28 +0100

    lineage: products: remove sdk_phone_x86 emulator target
    necessary after merge of tag 'android-14.0.0_r29':
    
     e04167e Retire obsolete (32bit-only) emulator targets and boards
    
     Please use 64bit-only targets instead:
     [...]  * we don't support x86+arm for AOSP targets
    
    Change-Id: I001b97e218f837005d4283bd0540519b00301783

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -20,7 +20,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ev_gsi_x86.mk \
     $(LOCAL_DIR)/ev_gsi_x86_64.mk \
     $(LOCAL_DIR)/ev_sdk_phone_arm64.mk \
-    $(LOCAL_DIR)/ev_sdk_phone_x86.mk \
     $(LOCAL_DIR)/ev_sdk_phone_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
@@ -29,5 +28,4 @@ COMMON_LUNCH_CHOICES := \
     ev_gsi_x86-userdebug \
     ev_gsi_x86_64-userdebug \
     ev_sdk_phone_arm64-userdebug \
-    ev_sdk_phone_x86-userdebug \
     ev_sdk_phone_x86_64-userdebug
