LineageOS/android_vendor_lineage

lineage: products: Bring back lineage_sdk_phone_arm64 emulator target
Change-Id: I0578608bd78e1a03f90b17de4ee558646ce9ef2f

build/target/product/AndroidProducts.mk
@@ -27,6 +27,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_gsi_tv_x86_64.mk \
     $(LOCAL_DIR)/lineage_sdk_car_arm64.mk \
     $(LOCAL_DIR)/lineage_sdk_car_x86_64.mk \
+    $(LOCAL_DIR)/lineage_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/lineage_sdk_phone_x86.mk \
     $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/lineage_sdk_tv_arm.mk \
@@ -45,6 +46,7 @@ COMMON_LUNCH_CHOICES := \
     lineage_gsi_tv_x86_64-userdebug \
     lineage_sdk_car_arm64-userdebug \
     lineage_sdk_car_x86_64-userdebug \
+    lineage_sdk_phone_arm64-userdebug \
     lineage_sdk_phone_x86-userdebug \
     lineage_sdk_phone_x86_64-userdebug \
     lineage_sdk_tv_arm-userdebug \

Evervolv/android_vendor_ev_build

target/product/AndroidProducts.mk
@@ -19,6 +19,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ev_gsi_arm64.mk \
     $(LOCAL_DIR)/ev_gsi_x86.mk \
     $(LOCAL_DIR)/ev_gsi_x86_64.mk \
+    $(LOCAL_DIR)/ev_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/ev_sdk_phone_x86.mk \
     $(LOCAL_DIR)/ev_sdk_phone_x86_64.mk
 
@@ -27,5 +28,6 @@ COMMON_LUNCH_CHOICES := \
     ev_gsi_arm64-userdebug \
     ev_gsi_x86-userdebug \
     ev_gsi_x86_64-userdebug \
+    ev_sdk_phone_arm64-userdebug \
     ev_sdk_phone_x86-userdebug \
     ev_sdk_phone_x86_64-userdebug