2d8e7998e6661f1a192aeb440d5ebbacc9255cf6
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-12 03:10:44 +0200

    kernel: Better x86 support
    - Mention the right kernel image on documentation
    - Build dtbs only when the dts folder exists, Google enables CONFIG_OF
      on x86_64 GKI config, this is also the same check done on Linux
      Makefile
    
    Change-Id: I1b0f7a39b0b00f2a4516d2cff79e591136ee3af8

--- tasks/kernel.mk
+++ tasks/kernel.mk
@@ -32,6 +32,7 @@
 #   BOARD_KERNEL_IMAGE_NAME            = Built image name
 #                                          for ARM use: zImage
 #                                          for ARM64 use: Image.gz
+#                                          for x86 use: bzImage
 #                                          for uncompressed use: Image
 #                                          If using an appended DT, append '-dtb'
 #                                          to the end of the image name.
@@ -444,7 +445,7 @@ endif
 $(TARGET_PREBUILT_INT_KERNEL): $(KERNEL_CONFIG) $(DEPMOD) $(DTC) $(LZ4)
 	@echo "Building Kernel Image ($(KERNEL_IMAGE_NAME))"
 	$(call make-kernel-target,$(KERNEL_IMAGE_NAME))
-	$(hide) if grep -q '^CONFIG_OF=y' $(KERNEL_CONFIG); then \
+	$(hide) if [ -d "$(KERNEL_SRC)/arch/$(KERNEL_ARCH)/boot/dts/" ]; then \
 			echo "Building DTBs"; \
 			$(call make-kernel-target,dtbs); \
 		fi
