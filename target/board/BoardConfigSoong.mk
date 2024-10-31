78ca546609cab8df9a35f97043adee12c01e4953
Luofan Chen <clfbbn@gmail.com>
2024-10-31 11:41:26 +0000

    soong: Unset custom charging path var if it is not defined
    Change-Id: I9de31c8aff5b17932f0de23026aebf95166c5163

--- target/board/BoardConfigSoong.mk
+++ target/board/BoardConfigSoong.mk
@@ -33,7 +33,6 @@ SOONG_CONFIG_evervolvGlobalVars += \
     disable_postrender_cleanup \
     has_legacy_camera_hal1 \
     has_memfd_backport \
-    target_health_charging_control_charging_path \
     target_health_charging_control_charging_enabled \
     target_health_charging_control_charging_disabled \
     target_health_charging_control_deadline_path \
@@ -52,6 +51,11 @@ SOONG_CONFIG_evervolvGlobalVars += \
     target_trust_usb_control_enable \
     target_trust_usb_control_disable \
     uses_legacy_contructor_map
+
+ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
+SOONG_CONFIG_evervolvGlobalVars += \
+    target_health_charging_control_charging_path
+endif
 
 SOONG_CONFIG_NAMESPACES += evervolvQcomVars
 SOONG_CONFIG_evervolvQcomVars += \
@@ -95,7 +99,9 @@ TARGET_QTI_USB_SUPPORTS_DEBUG_ACCESSORY ?= false
 SOONG_CONFIG_evervolvGlobalVars_disable_postrender_cleanup := $(TARGET_DISABLE_POSTRENDER_CLEANUP)
 SOONG_CONFIG_evervolvGlobalVars_has_legacy_camera_hal1 := $(TARGET_HAS_LEGACY_CAMERA_HAL1)
 SOONG_CONFIG_evervolvGlobalVars_has_memfd_backport := $(TARGET_HAS_MEMFD_BACKPORT)
+ifneq ($(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH),)
 SOONG_CONFIG_evervolvGlobalVars_target_health_charging_control_charging_path := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_PATH)
+endif
 SOONG_CONFIG_evervolvGlobalVars_target_health_charging_control_charging_enabled := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_ENABLED)
 SOONG_CONFIG_evervolvGlobalVars_target_health_charging_control_charging_disabled := $(TARGET_HEALTH_CHARGING_CONTROL_CHARGING_DISABLED)
 SOONG_CONFIG_evervolvGlobalVars_target_health_charging_control_deadline_path := $(TARGET_HEALTH_CHARGING_CONTROL_DEADLINE_PATH)
