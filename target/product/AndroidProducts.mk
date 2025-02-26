wodanesdag/android_vendor_crdroid

lineage: products: Add ATV x86_64 emulator targets
Change-Id: I2ab42a7800b9754e784b2bf5dad7e6fea839b8ff

build/target/product/AndroidProducts.mk
@@ -30,7 +30,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/lineage_sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/lineage_sdk_tv_arm.mk \
-    $(LOCAL_DIR)/lineage_sdk_tv_x86.mk
+    $(LOCAL_DIR)/lineage_sdk_tv_x86.mk \
+    $(LOCAL_DIR)/lineage_sdk_tv_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_gsi_arm-userdebug \
@@ -48,4 +49,5 @@ COMMON_LUNCH_CHOICES := \
     lineage_sdk_phone_arm64-userdebug \
     lineage_sdk_phone_x86_64-userdebug \
     lineage_sdk_tv_arm-userdebug \
-    lineage_sdk_tv_x86-userdebug
+    lineage_sdk_tv_x86-userdebug \
+    lineage_sdk_tv_x86_64-userdebug

Evervolv/android_vendor_ev_build

target/product/AndroidProducts.mk
@@ -26,7 +26,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ev_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/ev_sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/ev_sdk_tv_arm.mk \
-    $(LOCAL_DIR)/ev_sdk_tv_x86.mk
+    $(LOCAL_DIR)/ev_sdk_tv_x86.mk \
+    $(LOCAL_DIR)/ev_sdk_tv_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     ev_gsi_arm-userdebug \
@@ -40,4 +41,5 @@ COMMON_LUNCH_CHOICES := \
     ev_sdk_phone_arm64-userdebug \
     ev_sdk_phone_x86_64-userdebug \
     ev_sdk_tv_arm-userdebug \
-    ev_sdk_tv_x86-userdebug
+    ev_sdk_tv_x86-userdebug \
+    ev_sdk_tv_x86_64-userdebug
c3101db3b44969c4eb7eb36452c73df479a21a3b
Christian Hoffmann <chrmhoffmann@gmail.com>
2024-12-05 19:17:32 +0100

    lineage: products: Add ATV x86_64 emulator targets
    Change-Id: I2ab42a7800b9754e784b2bf5dad7e6fea839b8ff

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -26,7 +26,8 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/ev_sdk_phone_arm64.mk \
     $(LOCAL_DIR)/ev_sdk_phone_x86_64.mk \
     $(LOCAL_DIR)/ev_sdk_tv_arm.mk \
-    $(LOCAL_DIR)/ev_sdk_tv_x86.mk
+    $(LOCAL_DIR)/ev_sdk_tv_x86.mk \
+    $(LOCAL_DIR)/ev_sdk_tv_x86_64.mk
 
 COMMON_LUNCH_CHOICES := \
     ev_gsi_arm-userdebug \
@@ -40,4 +41,5 @@ COMMON_LUNCH_CHOICES := \
     ev_sdk_phone_arm64-userdebug \
     ev_sdk_phone_x86_64-userdebug \
     ev_sdk_tv_arm-userdebug \
-    ev_sdk_tv_x86-userdebug
+    ev_sdk_tv_x86-userdebug \
+    ev_sdk_tv_x86_64-userdebug
